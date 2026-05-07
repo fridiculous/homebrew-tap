838759b1c112a6d4b1fea89bb75789c25d09809b
Simon Frid <simon@simonfrid.com>
2026-05-07 09:46:47 -0600

    release: install rostrum from v0.1.0 binaries

--- Formula/rostrum.rb
+++ Formula/rostrum.rb
@@ -1,16 +1,26 @@
 class Rostrum < Formula
   desc "Global git worktree switchboard"
   homepage "https://github.com/fridiculous/rostrum"
-  url "https://github.com/fridiculous/rostrum/archive/5694896f9e3ccbd3e5ae74a0f5f41a4595112a7d.tar.gz"
-  sha256 "d8feb52cebed4a645e584ef4a8198decf0cfcc59279c3ac6e31af53b61de0f24"
+  version "0.1.0"
   license "MIT"
-  version "0.1.0-git"
-  head "https://github.com/fridiculous/rostrum.git", branch: "main"
 
-  depends_on "rust" => :build
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/fridiculous/rostrum/releases/download/v0.1.0/rostrum-aarch64-apple-darwin.tar.gz"
+      sha256 "f77fb2138a11a9864db323cbddd2f3eda33f51fe3d06d22c63c51a7f24c94c1f"
+    else
+      url "https://github.com/fridiculous/rostrum/releases/download/v0.1.0/rostrum-x86_64-apple-darwin.tar.gz"
+      sha256 "7929f4a15acee8f3bf97c7acb99a6ccf487248c4189a0fbf979be1b71cddd408"
+    end
+  end
+
+  on_linux do
+    url "https://github.com/fridiculous/rostrum/releases/download/v0.1.0/rostrum-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "cbbd730ed576b78ddf7d001ce5ef77fd2a4c01d4c75544980c20094db6f38ae6"
+  end
 
   def install
-    system "cargo", "install", *std_cargo_args(path: ".")
+    bin.install Dir["rsm-*"][0] => "rsm"
   end
 
   test do
