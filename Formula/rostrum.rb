55b80b94d574546f9140a8c0bf73451bd9052c41
Simon Frid <simon@simonfrid.com>
2026-05-07 09:33:42 -0600

    fix: pin tap formula to a real rostrum source archive

--- Formula/rostrum.rb
+++ Formula/rostrum.rb
@@ -1,9 +1,10 @@
 class Rostrum < Formula
   desc "Global git worktree switchboard"
   homepage "https://github.com/fridiculous/rostrum"
-  url "https://github.com/fridiculous/rostrum/archive/refs/heads/main.tar.gz"
-  sha256 "a83a80039d4290bd2b6b354171945e06f6323be459340dcb6b9027cb46bb7b1d"
+  url "https://github.com/fridiculous/rostrum/archive/5694896f9e3ccbd3e5ae74a0f5f41a4595112a7d.tar.gz"
+  sha256 "d8feb52cebed4a645e584ef4a8198decf0cfcc59279c3ac6e31af53b61de0f24"
   license "MIT"
+  version "0.1.0-git"
   head "https://github.com/fridiculous/rostrum.git", branch: "main"
 
   depends_on "rust" => :build
